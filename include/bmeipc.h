856c347e99bde192d28394c2c9028c538e0fb9e7
Markus Lehtonen <markus.lehtonen@nokia.com>
2010-10-01 17:38:02 +0300

    Add first version of the lib

--- include/bmeipc.h
+++ include/bmeipc.h
@@ -1,145 +1,144 @@
-/*
- * $Id: client_ipc.h 1988 2009-06-22 15:05:05Z spiiroin $
- *
- * Definitions for client/server IPC.
- */
+/**
+   @file bmeipc.h
 
-#ifndef _CLIENT_IPC
-#define _CLIENT_IPC
+   @brief BME IPC function interface
+   <p>
+   Copyright (C) 2010 Nokia Corporation.
 
-#include <stdarg.h>
+   @author Markus Lehtonen <markus.lehtonen@nokia.com>
+   @author Matti Halme <matti.halme@nokia.com>
 
-#define BME_SRV_SOCK_PATH	"/tmp/.bmesrv"
-#define BME_SRV_COOKIE		"BMentity"
-#define BME_SRV_MAX_REQUEST	512
-#define BME_SRV_CONN_TIMEOUT	1		/* seconds */
+   This file is part of libbme.
+
+   Libbme is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 2.1 as published by the Free Software Foundation.
+
+   Libbme is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with libbme. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BMEIPC_H
+#define BMEIPC_H
+
+#include <stdint.h>
+
+#define BME_SRV_SOCK_PATH "/tmp/.bmesrv"
+#define BME_SRV_COOKIE    "BMentity"
 
 /* System message codes (not related to battery management) */
-#define	BME_SYSMSG_GET_PID	 0x8000
-#define BME_SYSMSG_PROXY_OPEN    0x8001
-#define BME_SYSMSG_PROXY_CLOSE   0x8002
-#define BME_SYSMSG_PROXY_GETTIME 0x8003
+enum bme_sysmsg_e
+{
+  BME_SYSMSG_GETPID = 0x8000,   /* beyond ISI reuests range */
+  BME_SYSMSG_PROXY_OPEN,
+  BME_SYSMSG_PROXY_CLOSE,
+  BME_SYSMSG_PROXY_GETTIME      /* 0x8003 get bme statistics */
+};
 
-/*
- * Additional (non-system) message codes used by BME.
- * They must not overlap with constants from em_isi.h
+/** Generic message */
+typedef struct bmeipc_msg_s
+{
+  uint16_t type;
+  uint16_t subtype;
+} bmeipc_msg_t;
+
+/** Server PID reply */
+typedef struct bmeipc_pid_s
+{
+  uint32_t zero;
+  uint32_t pid;
+} bmeipc_pid_t;
+
+/**
+ * Receive BME data from socket
+ *
+ * @param fd socket descriptor
+ * @param msg data address
+ * @param bytes data size
+ *
+ * @return number of bytes read, -1 on error
+ *
+ * @ingroup bmeipc
  */
-#define BME_AUXMSG_TEST_MODE_CTRL	0x40	// Test mode control
-#define BME_AUXMSG_FTD_REQ		0x41	// FTD information request
-#define BME_AUXMSG_FTD_BULK_REQ		0x42	// FTD bulk information request
-#define BME_AUXMSG_FTD_BULK_REQ2	0x43	// FTD bulk information request2
-#define BME_AUXMSG_FTD_BULK_REQ3	0x44	// FTD bulk information request3
+int32_t bme_packet_read(int32_t fd, void *msg, int32_t bytes);
+int32_t bme_read(void *msg, int32_t bytes);
 
-#define BME_AUXMSG_EM_USB_MODE		0x48	// EM USB mode control
+/**
+ * Send BME data to socket.
+ *
+ * @param fd socket descriptor
+ * @param msg data address
+ * @param bytes data size
+ *
+ * @return  number of bytes send, -1 on error
+ *
+ * @ingroup bmeipc
+ */
+int32_t bme_packet_write(int32_t fd, const void *msg, int32_t bytes);
+int32_t bme_write(const void *msg, int32_t bytes);
 
-/* Test mode on/off */
-#define ACC_TEST_MODE_OFF		0
-#define ACC_TEST_MODE_ON		1
-
-/* Flags for EM FTD bulk data request */
-#define EM_FTD_REQ_FLAG_STATUS			1
-#define EM_FTD_REQ_FLAG_INST_BAT_VOLT		2
-#define EM_FTD_REQ_FLAG_REMAIN_STBY_MINUTES	4
-#define EM_FTD_REQ_FLAG_NUMBER_BAT_LOW_WARN	8
-#define EM_FTD_REQ_FLAG_MISC_MON_INT_FLAGS1	0x10
-#define EM_FTD_REQ_FLAG_CALC_VOLT_LEVEL		0x20
-#define EM_FTD_REQ_FLAG_WARN_COUNT		0x40
-#define EM_FTD_REQ_FLAG_DMF_BAT_VOLT		0x80
-#define EM_FTD_REQ_FLAG_BAT_BOOT_VOLT		0x100
-#define EM_FTD_REQ_FLAG_CALC_BAT_BAR		0x200
-#define EM_FTD_REQ_FLAG_VOLT_AT_FIRST_BAT_LOW	0x400
-#define EM_FTD_REQ_FLAG_CURRENT_MON_EST1	0x800
-#define EM_FTD_REQ_FLAG_CHARGED_AMOUNT		0x1000
-#define EM_FTD_REQ_FLAG_POWEROFF_LOG_RESERVED	0x2000	// Not supported yet
-#define EM_FTD_REQ_FLAG_LOWEST_V_TXOFF		0x4000
-#define EM_FTD_REQ_FLAG_LOWEST_V_TXON		0x8000
-#define EM_FTD_REQ_FLAG_LARGEST_DV		0x10000
-#define EM_FTD_REQ_FLAG_BATT_BAR_LOG_MASK	0x20000
-#define EM_FTD_REQ_FLAG_PREV_BATT_BAR_LEV	0x40000
-#define EM_FTD_REQ_FLAG_BATT_LOW_REASON		0x80000
-/* Added 21.6.05 */
-#define EM_FTD_REQ_FLAG_CS_STATE		0x100000
-#define EM_FTD_REQ_FLAG_NUM_OF_BAT_BARS		0x200000
-#define EM_FTD_REQ_FLAG_BAT_TYPE		0x400000
-#define EM_FTD_REQ_FLAG_CURRENT_BAT_TEMP	0x800000
-#define EM_FTD_REQ_FLAG_BAT_CAPACITY		0x1000000
-#define EM_FTD_REQ_FLAG_BAT_IMP			0x2000000
-#define EM_FTD_REQ_FLAG_BAT_FULL_THRES		0x4000000
-#define EM_FTD_REQ_FLAG_BAT_LOW_THRES		0x8000000
-#define EM_FTD_REQ_FLAG_MON_ALG_ADJ		0x10000000
-#define EM_FTD_REQ_FLAG_PARAM_ERRORS		0x20000000
-#define EM_FTD_REQ_FLAG_CURRENT_MON_EST2	0x40000000
-
-/* Flags for EM FTD bulk request2 */
-#define EM_FTD_REQ2_FLAG_EST_TX_OFF		1
-#define EM_FTD_REQ2_FLAG_EST_TX_ON		2
-#define EM_FTD_REQ2_FLAG_BAT_POW_IND		4
-#define EM_FTD_REQ2_FLAG_MISC_MON_INT_FLAGS2	8
-#define EM_FTD_REQ2_FLAG_MISC_MON_INT_FLAGS3	0x10
-#define EM_FTD_REQ2_FLAG_MON_VOLT		0x20
-#define EM_FTD_REQ2_FLAG_ELAPSED_TIME		0x40
-#define EM_FTD_REQ2_FLAG_PHI_VARIABLE		0x80
-#define EM_FTD_REQ2_FLAG_DELTA_PHI_VARIABLE	0x100
-#define EM_FTD_REQ2_FLAG_CHA_METHOD		0x200
-#define EM_FTD_REQ2_FLAG_PRESENT_CHA_MODE	0x400
-#define EM_FTD_REQ2_FLAG_PREVIOUS_CHA_MODE	0x800
-#define EM_FTD_REQ2_FLAG_PRESENT_CHR_TYPE	0x1000
-#define EM_FTD_REQ2_FLAG_PREVIOUS_CHR_TYPE	0x2000
-#define EM_FTD_REQ2_FLAG_CHA_INST_BAT_VOLT	0x4000
-#define EM_FTD_REQ2_FLAG_NUMBER_CHR_CHECKS	0x8000
-#define EM_FTD_REQ2_FLAG_CHR_REC_STATE		0x10000
-#define EM_FTD_REQ2_FLAG_INST_CHR_CURRENT	0x20000
-#define EM_FTD_REQ2_FLAG_BAT_FULL		0x40000
-#define EM_FTD_REQ2_FLAG_CHA_TIME		0x80000
-#define EM_FTD_REQ2_FLAG_AVERAGE_CHR_VOLT	0x100000
-#define EM_FTD_REQ2_FLAG_CALC_CHR_CURRENT	0x200000
-#define EM_FTD_REQ2_FLAG_HW_PWM_VALUE		0x400000
-#define EM_FTD_REQ2_FLAG_SW_PWM_VALUE		0x800000
-#define EM_FTD_REQ2_FLAG_OS_PWM_BAT_VOLT	0x1000000
-#define EM_FTD_REQ2_FLAG_CS_PWM_BAT_VOLT	0x2000000
-
-/* Flags for EM FTD bulk request3 */
-#define	EM_FTD_REQ3_FLAG_CONF_BAT_FOOTPRINT	1
-#define	EM_FTD_REQ3_FLAG_CONF_BAT_BARS		2
-#define	EM_FTD_REQ3_FLAG_CONF_MIN_CURRENT	4
-#define	EM_FTD_REQ3_FLAG_CONF_BAT_SAFETY_LEVEL	8
-#define	EM_FTD_REQ3_FLAG_CONF_BAT_EMPTY_LEVEL	0x10
-
-/*** General messages *********************************************************/
-
-/* Generic message */
-typedef struct {
-    uint16	type, subtype;
-} tBMEmsgGeneric;
-
-/* Server PID reply */
-typedef struct {
-    uint32	zero;
-    uint32	pid;
-} tBMEreplyServerPID;
-
-typedef union {
-    tBMEmsgGeneric request;
-    tBMEreplyServerPID reply;
-} tBMEmsgServerPID;
-
-
-/*** Function prototypes ******************************************************/
-
-/* Client-side functions */
-int bme_connect(void);
-int bme_get_server_pid(void);
+/**
+ * Close connection to BME server
+ *
+ * @ingroup bmeipc
+ */
 void bme_disconnect(void);
-int bme_write(const void *msg, int bytes);
-int bme_read(void *msg, int bytes);
-int bme_send_get_reply(const void *smsg, int sbytes,
-		       void *rmsg, int rbytes, int *rbytes_act);
-int bme_packet_write(int fd, const void *msg, int bytes);
-int bme_packet_read(int fd, void *msg, int bytes);
 
-int bme_cookie_write(int fd, const char *cookie);
-int bme_cookie_read(int fd, const char *cookie);
+/**
+ * Open connection to BME server
+ *
+ * @ingroup bmeipc
+ *
+ * @return socket descriptor on success, -1 on error
+ */
+int32_t bme_connect(void);
 
-void bme_set_logging_function(void (*fn)(int,const char *,va_list));
+/* -------------------- BME messaging primitives -------------------- */
 
-#endif
+/** Send message to the server and get reply
+ *
+ * @param smsg address of a message to send
+ * @param sbytes size of message to send
+ * @param rmsg address of a reply buffer
+ * @param rbytes size of reply buffer
+ * @rbytes_act: actual size of reply got from the server
+ *
+ * @return  >= 0 on success, -1 on error
+ *    NB: if rmsg is NULL reply status is returned
+ */
+int32_t bme_send_get_reply(const void *smsg, int32_t sbytes,
+                           void *rmsg, int32_t rbytes, int32_t * rbytes_act);
+
+/**
+ * Get a PID of BME server.
+ *
+ * @return  PID on success, -1 on error
+ */
+int32_t bme_get_server_pid(void);
+
+/**
+ * Read BME cookie
+ *
+ * @param fd socket descriptor to receive cookie from
+ * @param cookie cookie
+ *
+ * @return  0 on success, -1 on error
+ */
+int32_t bme_cookie_read(int32_t fd, const char *cookie);
+
+/**
+ * Write BME cookie
+ *
+ * @param fd socket descriptor to send cookie to
+ * @param cookie cookie
+ *
+ * @return  0 on success, -1 on error
+ */
+int32_t bme_cookie_write(int32_t fd, const char *cookie);
+
+#endif /* BMEIPC_H */
--- src/bmeipc.c
+++ src/bmeipc.c
@@ -1,38 +1,52 @@
-/*
- * $Id: bmeipc.c 1948 2009-06-12 11:08:31Z spiiroin $
- *
- * Library for communicating with BME.
- */
+/**
+   @file bmeipc.c
+
+   @brief BME IPC function interface
+   <p>
+   Copyright (C) 2010 Nokia Corporation.
+
+   @author Markus Lehtonen <markus.lehtonen@nokia.com>
+   @author Matti Halme <matti.halme@nokia.com>
+
+   This file is part of libbme.
+
+   Libbme is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 2.1 as published by the Free Software Foundation.
+
+   Libbme is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with libbme. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
-#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <string.h>
-#include <poll.h>
 #include <sys/syslog.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/poll.h>
-#include <sys/uio.h>
+#include <stdarg.h>
 
 #include <time.h>
 #include <sys/time.h>
+#include <stdint.h>
 
-#include "global.h"
-#include "type_def.h"
-
-#include "osso/client_ipc.h"
+#include "bmeipc.h"
 
 /**
  * Get time stamp that is not affected by system time changes 
  */
-static void getmonotime(struct timeval *tv)
+static void
+getmonotime(struct timeval *tv)
 {
   struct timespec t;
-  if( clock_gettime(CLOCK_MONOTONIC, &t) == 0 )
+  if (clock_gettime(CLOCK_MONOTONIC, &t) == 0)
   {
     TIMESPEC_TO_TIMEVAL(tv, &t);
   }
@@ -45,14 +59,14 @@ static void getmonotime(struct timeval *tv)
 /**
  * Set timeout given milliseconds in to future 
  */
-static void settimeout(struct timeval *timeout, int msec)
+static void
+settimeout(struct timeval *timeout, int msec)
 {
-  struct timeval s =
-  {
-    .tv_sec  = (msec / 1000),
+  struct timeval s = {
+    .tv_sec = (msec / 1000),
     .tv_usec = (msec % 1000) * 1000,
   };
-  
+
   getmonotime(timeout);
   timeradd(timeout, &s, timeout);
 }
@@ -60,19 +74,20 @@ static void settimeout(struct timeval *timeout, int msec)
 /**
  * Return milliseconds left to timeout 
  */
-static int msecsto(const struct timeval *timeout)
+static int
+msecsto(const struct timeval *timeout)
 {
   struct timeval t;
   int msec = 0;
-  
+
   getmonotime(&t);
-  
-  if( timercmp(&t, timeout, <) )
+
+  if (timercmp(&t, timeout, <))
   {
     timersub(timeout, &t, &t);
     msec = t.tv_sec * 1000 + t.tv_usec / 1000;
   }
-  
+
   return msec;
 }
 
@@ -85,92 +100,21 @@ static int bme_sd = -1;
 /**
  * BME packet header structure
  */
-typedef struct {
-    int sync; // sync pattern for detecting broken packets
-    int size; // size of actual packet data after the header
+typedef struct
+{
+  int sync;                     // sync pattern for detecting broken packets
+  int size;                     // size of actual packet data after the header
 } bmeipc_header;
-
-/**
- * Pointer to vsyslog() compatible logging function used by bmeipc.
- */
-static void (*log_message_fn)(int,const char *,va_list) = vsyslog;
 
 /**
  * Sync pattern for packet header
  */
 #define BMEIPC_SYNCWORD 0x434e5953
 
-#ifdef DEAD_CODE
 /**
- * Error diagnostics output
- * 
- * Note: The errno value will not be modified by this function.
- *
- * @level: syslog level constant (LOG_WARNING etc)
- * @fmt:   printf style format string
- * @va:    appropriate arguments for the format string
+ * Pointer to vsyslog() compatible logging function used by bmeipc.
  */
-static void log_message_va(int level, const char *fmt, va_list va)
-{
-  int saved = errno;
-  char exe[64];
-  char msg[256];
-  va_list va;
-  const char *lev = "log";
-  unsigned char *s, *d, *e;
-  ssize_t n;
-  
-  switch(level) {
-  case LOG_DEBUG:   lev = "debug";   break;
-  case LOG_INFO:    lev = "info";    break;
-  case LOG_NOTICE:  lev = "notice";  break;
-  case LOG_WARNING: lev = "warning"; break;
-  case LOG_ERR:     lev = "error";   break;
-  }
-
-  /* Get process name from /proc */
-  n = readlink("/proc/self/exe", exe, sizeof exe - 1);
-  exe[(n > 0 && n < sizeof exe) ? n : 0] = 0;
-  
-  /* Restore errno for '%m' in case readlink failed */
-  errno = saved;
-  
-  /* Format the message */
-  vsnprintf(msg, sizeof msg, fmt, va);
-
-  /* Get rid of trailing whitespace */
-  for( s = d = e = (unsigned char *)msg; *s; ++s )
-  {
-    if( *s <= ' ' ) *d++ = ' ';
-    else            *d++ = *s, e = d;
-  }
-  *e = 0;
-
-  /* Write the message to stderr */
-  fprintf(stderr, "%s: BMEIPC: %s: %s\n", basename(exe), lev, msg);
-  fflush(stderr);
-  errno = saved;
-}
-#endif
-
-/**
- * Set function used for bmeipc logging.
- * 
- * Defaults to using vsyslog. Custom function should
- * have the same calling convention as vyslog, i.e.
- * 
- * void custom(int priority, const char *format, va_list ap);
- * 
- * Passing NULL is allowed and makes bmeipc totally silent.
- * 
- * @fn: pointer to function similar to vsyslog.
- */
-
-void bme_set_logging_function(void (*fn)(int,const char *,va_list))
-{
-  log_message_fn = fn;
-}
-
+static void (*log_message_fn) (int, const char *, va_list) = vsyslog;
 
 /**
  * Error diagnostics output
@@ -183,11 +127,12 @@ void bme_set_logging_function(void (*fn)(int,const char *,va_list))
  */
 
 static void log_message(int level, const char *fmt, ...)
-                __attribute__((format(printf,2,3)));
+  __attribute__ ((format(printf, 2, 3)));
 
-static void log_message(int level, const char *fmt, ...)
+static void
+log_message(int level, const char *fmt, ...)
 {
-  if( log_message_fn != 0 )
+  if (log_message_fn != 0)
   {
     va_list va;
     va_start(va, fmt);
@@ -211,46 +156,49 @@ static void log_message(int level, const char *fmt, ...)
  *
  * @return number of bytes read, 0=EOF, -1=ERR
  */
-static int bme_bytes_read(int fd, void *data, int size)
+static int
+bme_bytes_read(int fd, void *data, int size)
 {
-  struct pollfd  pfd = { .fd = fd, .events = POLLIN };
+  struct pollfd pfd = {.fd = fd,.events = POLLIN };
   struct timeval tmo;
-  
+
   int rc;
-  
+
   /* Wait max 5 secs for data / EOF to come available */
   settimeout(&tmo, 5000);
   rc = TEMP_FAILURE_RETRY(poll(&pfd, 1, msecsto(&tmo)));
-  
-  if( rc == -1 ) {
+
+  if (rc == -1)
+  {
     log_warn_F("[fd=%d] poll ERROR: %s\n", fd, strerror(errno));
     return -1;
   }
-  
-  if( rc == 0 ) {
+
+  if (rc == 0)
+  {
     // set errno to something meaningful
     errno = ETIMEDOUT;
     log_warn_F("[fd=%d] poll TIMEOUT\n", fd);
     return -1;
   }
-  
+
   /* Read the data that is available immediately, but do not
    * block if less than expected is ready for reading */
   rc = TEMP_FAILURE_RETRY(recv(fd, data, size, MSG_DONTWAIT));
-  
-  if( rc == -1 )
+
+  if (rc == -1)
   {
     log_warn_F("[fd=%d] read ERROR: %s\n", fd, strerror(errno));
   }
-  else if( rc == 0 )
+  else if (rc == 0)
   {
     //log_warn_F("[fd=%d] read EOF\n", fd);
   }
-  else if( rc != size )
+  else if (rc != size)
   {
     log_warn_F("[fd=%d] read ERROR: %d/%d bytes\n", fd, rc, size);
   }
-  
+
   return rc;
 }
 
@@ -263,28 +211,28 @@ static int bme_bytes_read(int fd, void *data, int size)
  *
  * @return number of bytes written, -1=Error
  */
-int bme_packet_write(int fd, const void *msg, int bytes)
+int
+bme_packet_write(int fd, const void *msg, int bytes)
 {
-  bmeipc_header hdr =
-  {
+  bmeipc_header hdr = {
     .sync = BMEIPC_SYNCWORD,
     .size = bytes,
   };
 
-  struct iovec iov[2] =
-  {
-    { .iov_base = &hdr,        .iov_len = sizeof hdr },
-    { .iov_base = (void*)msg,  .iov_len = bytes },
+  struct iovec iov[2] = {
+    {.iov_base = &hdr,.iov_len = sizeof hdr}
+    ,
+    {.iov_base = (void *)msg,.iov_len = bytes},
   };
 
   int tot = sizeof hdr + bytes;
   int ret = writev(fd, iov, 2);
 
-  if( ret == -1 )
+  if (ret == -1)
   {
     log_warn_F("[fd=%d]: write ERROR: %s\n", fd, strerror(errno));
   }
-  else if( ret != tot )
+  else if (ret != tot)
   {
     log_warn_F("[fd=%d]: write ERROR: %d/%d bytes\n", fd, ret, tot);
     // set errno to something meaningful
@@ -307,37 +255,38 @@ int bme_packet_write(int fd, const void *msg, int bytes)
  * @return size of packet following the header, -1=Error, 0=EOF/out-of-sync
  *
  */
-static int bme_header_read(int fd)
+static int
+bme_header_read(int fd)
 {
   bmeipc_header head;
 
   int done = bme_bytes_read(fd, &head, sizeof head);
 
-  if( done == -1 )
+  if (done == -1)
   {
     log_warn_F("[fd=%d]: read header: %s\n", fd, strerror(errno));
     return -1;
   }
-  if( done == 0 )
+  if (done == 0)
   {
     //log_warn_F("[fd=%d]: read header: %s\n", fd, "EOF");
-    return 0; // EOF
+    return 0;                   // EOF
   }
-  if( done != sizeof head )
+  if (done != sizeof head)
   {
     log_warn_F("[fd=%d]: read header: got %d / %Zd bytes\n",
-	     fd, done, sizeof head);
-    return 0; // EOF
+               fd, done, sizeof head);
+    return 0;                   // EOF
   }
-  if( head.sync != BMEIPC_SYNCWORD )
+  if (head.sync != BMEIPC_SYNCWORD)
   {
     log_warn_F("[fd=%d]: read header: %s\n", fd, "out of sync");
-    return 0; // EOF
+    return 0;                   // EOF
   }
-  if( head.size < 0 )
+  if (head.size < 0)
   {
     log_warn_F("[fd=%d]: read header: %s\n", fd, "negative size");
-    return 0; // EOF
+    return 0;                   // EOF
   }
 
   return head.size;
@@ -352,31 +301,32 @@ static int bme_header_read(int fd)
  *
  * @return number of bytes read, -1=ERR, 0=EOF/out-of-sync
  */
-int bme_packet_read(int fd, void *msg, int bytes)
+int
+bme_packet_read(int fd, void *msg, int bytes)
 {
   int ret;
 
-  if( (ret = bme_header_read(fd)) <= 0 )
+  if ((ret = bme_header_read(fd)) <= 0)
   {
-    return ret; // ERR or EOF
+    return ret;                 // ERR or EOF
   }
-  if( bytes < ret )
+  if (bytes < ret)
   {
     log_warn_F("[fd=%d]: read packet: got %d, expected max %d bytes\n",
-	       fd, ret, bytes);
+               fd, ret, bytes);
     // set errno to something meaningful
     errno = EBADMSG;
     return -1;
   }
-  if( bytes > ret )
+  if (bytes > ret)
   {
     bytes = ret;
   }
-  if( (ret = bme_bytes_read(fd, msg, bytes)) != bytes )
+  if ((ret = bme_bytes_read(fd, msg, bytes)) != bytes)
   {
     log_warn_F("[fd=%d]: read packet: got %d/%d bytes\n", fd, ret, bytes);
-    
-    if( ret != -1 )
+
+    if (ret != -1)
     {
       // set errno to something meaningful
       errno = EBADMSG;
@@ -398,40 +348,46 @@ int bme_packet_read(int fd, void *msg, int bytes)
 int
 bme_cookie_read(int fd, const char *cookie)
 {
-  int   error = -1;
-  int   todo  = strlen(cookie);
+  int error = -1;
+  int todo = strlen(cookie);
   char *magic = alloca(todo);
-  int   done  = 0;
+  int done = 0;
 
   // read cookie string
   done = bme_packet_read(fd, magic, todo);
-  if( done == -1 ) {
+  if (done == -1)
+  {
     log_warn_F("read cookie: %s\n", strerror(errno));
     goto cleanup;
   }
-  if( done != todo ) {
+  if (done != todo)
+  {
     log_warn_F("read cookie: got %d of %d bytes", done, todo);
     goto cleanup;
   }
-  if( strncmp(magic, cookie, todo) ) {
-    log_warn_F("cookie mismatch: got %*s, expected %s\n", done, magic, cookie);
+  if (strncmp(magic, cookie, todo))
+  {
+    log_warn_F("cookie mismatch: got %*s, expected %s\n", done, magic,
+               cookie);
     goto cleanup;
   }
-  
+
   // write ack byte
   done = bme_packet_write(fd, "\n", 1);
-  if( done == -1 ) {
+  if (done == -1)
+  {
     log_warn_F("write ack: %s", strerror(errno));
     goto cleanup;
   }
-  if( done != 1 ) {
+  if (done != 1)
+  {
     log_warn_F("write ack: did %d of %d bytes", done, 1);
     goto cleanup;
   }
 
   error = 0;
 
-  cleanup:
+cleanup:
   return error;
 }
 
@@ -447,36 +403,40 @@ bme_cookie_read(int fd, const char *cookie)
 int
 bme_cookie_write(int fd, const char *cookie)
 {
-  int  error   = -1;
-  int  todo    = strlen(cookie);
-  int  done    = 0;
-  char ack     = 0;
-  
+  int error = -1;
+  int todo = strlen(cookie);
+  int done = 0;
+  char ack = 0;
+
   // write cookie string
   done = bme_packet_write(fd, cookie, todo);
-  if( done == -1 ) {
+  if (done == -1)
+  {
     log_warn_F("write cookie: %s\n", strerror(errno));
     goto cleanup;
   }
-  if( done != todo ) {
+  if (done != todo)
+  {
     log_warn_F("write cookie: did %d of %d bytes\n", done, todo);
     goto cleanup;
   }
-  
+
   // read ack byte
-  done = bme_packet_read(fd, &ack,1);
-  if( done == -1 ) {
+  done = bme_packet_read(fd, &ack, 1);
+  if (done == -1)
+  {
     log_warn_F("read ack: %s\n", strerror(errno));
     goto cleanup;
   }
-  if( done != 1 ) {
+  if (done != 1)
+  {
     log_warn_F("read ack: got %d of %d bytes\n", done, 1);
     goto cleanup;
   }
-  
+
   error = 0;
-  
-  cleanup:
+
+cleanup:
   return error;
 }
 
@@ -485,28 +445,31 @@ bme_cookie_write(int fd, const char *cookie)
  *
  * @return socket descriptor if successful, -1=Error
  */
-int bme_connect(void)
+int
+bme_connect(void)
 {
-  static const char path[]   = BME_SRV_SOCK_PATH;
+  static const char path[] = BME_SRV_SOCK_PATH;
   static const char cookie[] = BME_SRV_COOKIE;
 
-  int result = -1; // assume failure
-  
+  int result = -1;              // assume failure
+
   struct sockaddr_un addr;
   socklen_t asize;
-  
+
   /* Check that we are not already connected */
-  if( bme_sd != -1 ) {
+  if (bme_sd != -1)
+  {
     // set errno to something meaningful
     errno = EALREADY;
     log_error_F("already connected\n");
     // return error without disconnecting
     return -1;
   }
-  
+
   /* Create socket */
   bme_sd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (bme_sd == -1) {
+  if (bme_sd == -1)
+  {
     log_error_F("socket: %s\n", strerror(errno));
     goto cleanup;
   }
@@ -517,20 +480,23 @@ int bme_connect(void)
   addr.sun_family = AF_UNIX;
   asize = sizeof addr;
 
-  if( connect(bme_sd, (struct sockaddr*) &addr, asize) == -1 ) {
-    /*log_error_F("connect: %s\n", strerror(errno));*/
+  if (connect(bme_sd, (struct sockaddr *)&addr, asize) == -1)
+  {
+    /*log_error_F("connect: %s\n", strerror(errno)); */
     goto cleanup;
   }
 
-  if( bme_cookie_write(bme_sd, cookie) == -1 ) {
+  if (bme_cookie_write(bme_sd, cookie) == -1)
+  {
     goto cleanup;
   }
   /* If we get here, the connection was succesfully established */
   result = bme_sd;
-  
-  cleanup:
-  
-  if( result == -1 ) {
+
+cleanup:
+
+  if (result == -1)
+  {
     bme_disconnect();
   }
   return result;
@@ -547,24 +513,27 @@ int bme_connect(void)
  *
  * @return status value, set by the server, or -1=Error
  */
-int bme_send_get_reply(const void *smsg, int sbytes,
-			void *rmsg, int rbytes, int *rbytes_act)
+int
+bme_send_get_reply(const void *smsg, int sbytes,
+                   void *rmsg, int rbytes, int *rbytes_act)
 {
-    int status, nb;
+  int status, nb;
 
-    if (bme_write(smsg, sbytes) != sbytes)
-	    return -1;
+  if (bme_write(smsg, sbytes) != sbytes)
+    return -1;
 
-    if (bme_read(&status, sizeof(status)) == -1)
-	    return -1;
+  if (bme_read(&status, sizeof(status)) == -1)
+    return -1;
 
-    if (status >= 0 && rmsg && rbytes) {
-	nb = bme_read(rmsg, rbytes);
-	if (nb == -1)
-		return -1;
-	if (rbytes_act) *rbytes_act = nb;
-    }
-    return status;
+  if (status >= 0 && rmsg && rbytes)
+  {
+    nb = bme_read(rmsg, rbytes);
+    if (nb == -1)
+      return -1;
+    if (rbytes_act)
+      *rbytes_act = nb;
+  }
+  return status;
 }
 
 /**
@@ -575,9 +544,11 @@ int bme_send_get_reply(const void *smsg, int sbytes,
  *
  * @return number of bytes written if successful, -1=Error
  */
-int bme_write(const void *msg, int bytes)
+int
+bme_write(const void *msg, int bytes)
 {
-  if( bme_sd == -1 ) {
+  if (bme_sd == -1)
+  {
     // set errno to something meaningful
     errno = ENOTCONN;
     return -1;
@@ -593,9 +564,11 @@ int bme_write(const void *msg, int bytes)
  *
  * @return number of bytes read if successful, -1=Error
  */
-int bme_read(void *msg, int bytes)
+int
+bme_read(void *msg, int bytes)
 {
-  if( bme_sd == -1 ) {
+  if (bme_sd == -1)
+  {
     // set errno to something meaningful
     errno = ENOTCONN;
     return -1;
@@ -608,26 +581,30 @@ int bme_read(void *msg, int bytes)
  *
  * @return positive PID if successful, -1=Error
  */
-int bme_get_server_pid(void)
+int
+bme_get_server_pid(void)
 {
-    tBMEmsgGeneric gm = { BME_SYSMSG_GET_PID, 0 };
-    tBMEreplyServerPID reply;
+  bmeipc_msg_t gm = { BME_SYSMSG_GETPID, 0 };
+  bmeipc_pid_t reply;
 
-    if (bme_send_get_reply(&gm, sizeof(gm), &reply, sizeof(reply), NULL) < 0)
-	return -1;
-    else
-	return reply.pid;
+  if (bme_send_get_reply(&gm, sizeof(gm), &reply, sizeof(reply), NULL) < 0)
+    return -1;
+  else
+    return reply.pid;
 }
 
 /**
  * Disconnect from BME server.
  */
-void bme_disconnect(void)
+void
+bme_disconnect(void)
 {
-  if( bme_sd != -1 ) {
-      if( TEMP_FAILURE_RETRY(close(bme_sd)) == -1 ) {
-	  log_warn_F("close: %s\n", strerror(errno));
-      }
-      bme_sd = -1;
+  if (bme_sd != -1)
+  {
+    if (TEMP_FAILURE_RETRY(close(bme_sd)) == -1)
+    {
+      log_warn_F("close: %s\n", strerror(errno));
+    }
+    bme_sd = -1;
   }
 }
